66af0356473b3d013f31eefc9ce6ffd0d4127a3d
A~H <74129604+alinumbercrunch@users.noreply.github.com>
2024-10-01 11:14:22 +0900

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,9 +16,9 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000/api/v1/restaurants'
-    resource '/restaurants',
-      :headers => :any,
-      :methods => [:post]
+    origins '*'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
